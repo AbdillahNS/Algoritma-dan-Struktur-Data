6691c61fc77591d3aca78c43f49db20d20f72daf
abdillah <abdillahnoersaid@gmail.com>
2024-02-21 12:02:37 +0700

    Jobsheet 2

---
+++ P2/Buku.java
@@ -0,0 +1,24 @@
+public class Buku {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: RP " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
---
+++ P2/BukuMain01.java
@@ -0,0 +1,15 @@
+public class BukuMain01 {
+    public static void main(String[] args) {
+        Buku bk1 = new Buku();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
